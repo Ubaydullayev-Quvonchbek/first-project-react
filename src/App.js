054a2ffb0aebe3cc428d3bb3e2cee0de1ff3f91f
Quvonchbek048 <quvonchbeku048@gmail.com>
2023-04-11 18:43:31 +0500

    Team Quvonchbek

--- src/App.js
+++ src/App.js
@@ -2,29 +2,12 @@ import React, { useState } from 'react';
 import './App.css';
 function App() {
   const [Count, setCount] = useState(0);
-  function increment() {
-    setCount(function (n) {
-      return n += 1
-    })
-  }
-  function decrement() {
-    setCount(function (n) {
-      if (n > 0) {
-        return (n -= 1);
-      } else {
-        return (n = 0);
-      }
-    })
-  }
-  function submit() {
-    Count = 0
-  }
   return (
     <>
       <h1>{Count}</h1>
-      <button className='incdec r1' onClick={() => increment()}>+</button>
-      <button className='incdec r2' onClick={() => decrement()}>-</button>
-      <button className='submit' onClick={() => submit()}>RESET</button>
+      <button className='incdec r1' onClick={() => setCount(Count + 1)}>+</button>
+      <button className='incdec r2' onClick={() => setCount(Count < 1 ? 0 : Count - 1)}>-</button>
+      <button className='reset' onClick={() => setCount(0)}>RESET</button>
     </>
   );
 }
