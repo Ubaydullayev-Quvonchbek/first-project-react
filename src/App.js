ab091e1e95a5e7e61e4fae7c0e767d07c2f96b4b
Quvonchbek048 <quvonchbeku048@gmail.com>
2023-04-25 17:15:18 +0500

    Team Quvonchbek

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1>{Count}</h1>
-      <button className='incdec r1' onClick={() => setCount(Count + 1)}>+</button>
+      <button className='incdec r1' onClick={() => Count >= 99 ? setCount(0) : setCount(Count + 1)}>+</button>
       <button className='incdec r2' onClick={() => setCount(Count < 1 ? 0 : Count - 1)}>-</button>
       <button className='reset' onClick={() => setCount(0)}>RESET</button>
     </>
